451e7007860c9154d3f41d015ab4c730d3816e42
Morgan Giraud <morgan@explee.com>
2016-11-05 21:43:52 +0100

    Add bee_simple

--- bee_simple.py
+++ bee_simple.py
@@ -6,26 +6,17 @@ from scipy.misc import imread, imresize
 
 dir = os.path.dirname(os.path.realpath(__file__))
 
-print('Loading vgg_graph')
-vgg_saver = tf.train.import_meta_graph(dir + '/vgg/results/vgg-16.meta')
-vgg_graph = tf.get_default_graph()
-
-x_plh = vgg_graph.get_tensor_by_name('input:0')
-conv5_3 =vgg_graph.get_tensor_by_name('conv5_3:0')
-
 print('Building bee graph')
 with tf.variable_scope("placeholder"):
+    x_plh = tf.placeholder(tf.int32, shape=[None, 224, 224, 3])
     y_plh = tf.placeholder(tf.int32, shape=[None, 1])
     y_true_reshaped = tf.reshape(y_plh, [-1])
 
 # First compresse channel wise
 with tf.variable_scope("bee_conv"):
-    W1 = tf.get_variable('W1', shape=[1, 1, 512, 32], initializer=tf.random_normal_initializer(stddev=1e-1))
-    tf.add_to_collection("bee_vars", W1)
+    W1 = tf.get_variable('W1', shape=[3, 3, 3, 32], initializer=tf.random_normal_initializer(stddev=1e-1))
     b1 = tf.get_variable('b1', shape=[32], initializer=tf.constant_initializer(0.1))
-    tf.add_to_collection("bee_vars", b1)
-
-    z1 = tf.nn.conv2d(conv5_3, W1, strides=[1, 1, 1, 1], padding='SAME') + b1
+    z1 = tf.nn.conv2d(x_plh, W1, strides=[1, 1, 1, 1], padding='SAME') + b1
     a = tf.nn.relu(z1)
 
 with tf.variable_scope("classifier"):
@@ -34,16 +25,12 @@ with tf.variable_scope("classifier"):
     a_vec = tf.reshape(a, [-1, a_vec_size])
 
     W_fc1 = tf.get_variable('W_fc1', shape=[a_vec_size, 100], initializer=tf.random_normal_initializer(stddev=1e-1))
-    tf.add_to_collection("bee_vars", W_fc1)
     b_fc1 = tf.get_variable('b_fc1', shape=[100], initializer=tf.constant_initializer(0.1))
-    tf.add_to_collection("bee_vars", b_fc1)
     z = tf.matmul(a_vec, W_fc1) + b_fc1
     a = tf.nn.relu(z)
 
     W_fc2 = tf.get_variable('W_fc2', shape=[100, 2], initializer=tf.random_normal_initializer(stddev=1e-1))
-    tf.add_to_collection("bee_vars", W_fc2)
     b_fc2 = tf.get_variable('b_fc2', shape=[2], initializer=tf.constant_initializer(0.1))
-    tf.add_to_collection("bee_vars", b_fc2)
     a = tf.matmul(a, W_fc2) + b_fc2
 
 with tf.variable_scope('loss'):
@@ -53,7 +40,7 @@ with tf.variable_scope('loss'):
 
 global_step = tf.Variable(initial_value=0, name='global_step', trainable=False)
 adam = tf.train.AdamOptimizer(learning_rate=1e-3)
-train_op = adam.minimize(total_loss, var_list=tf.get_collection('bee_vars'), global_step=global_step)
+train_op = adam.minimize(total_loss, global_step=global_step)
 
 train_summaries_op = tf.merge_all_summaries()
 
@@ -129,7 +116,6 @@ if __name__ == '__main__':
     with tf.Session() as sess:
         print("Init models")
         sess.run(tf.initialize_all_variables())
-        vgg_saver.restore(sess, dir + '/vgg/results/vgg-16')
 
         print('Bookeeping funcs')
         sw = tf.train.SummaryWriter(result_folder, sess.graph)
